f189ef17e8c2715fcd75098da5c3860d8ede13d3
AntonioArtigas <48927276+AntonioArtigas@users.noreply.github.com>
2022-10-29 19:51:23 -0400

    Add visitor methods to both Expressions and Statements.

--- src/main/scala/ast/Expr.scala
+++ src/main/scala/ast/Expr.scala
@@ -5,44 +5,86 @@ import tokenizer.Token
 
 /** Things that produce values of use
   */
-sealed abstract class Expr
+sealed abstract class Expr {
+  def accept[R](visitor: Expr.Visitor[R]): R
+}
 
 object Expr {
+  trait Visitor[R] {
+    def visitBinary(expr: Binary): R = ???
+    def visitIndexGet(expr: IndexGet): R = ???
+    def visitGetLength(expr: GetLength): R = ???
+    def visitCall(expr: Call): R = ???
+    def visitInteger(expr: Integer): R = ???
+    def visitBool(expr: Bool): R = ???
+    def visitVariable(expr: Variable): R = ???
+    def visitThis(expr: This): R = ???
+    def visitNewArray(expr: NewArray): R = ???
+    def visitNewInstance(expr: NewInstance): R = ???
+    def visitUnary(expr: Unary): R = ???
+    def visitGrouping(expr: Grouping): R = ???
+  }
+
   // foo <op> bar
-  case class Binary(left: Expr, op: BinaryOp, right: Expr) extends Expr
+  case class Binary(left: Expr, op: BinaryOp, right: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitBinary(this)
+  }
 
   // foo[<expr>]
-  case class IndexGet(obj: Expr, index: Expr) extends Expr
+  case class IndexGet(obj: Expr, index: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitIndexGet(this)
+  }
 
   // foo.length
-  case class GetLength(obj: Expr) extends Expr
+  case class GetLength(obj: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitGetLength(this)
+  }
 
   // foo.bar(<args>)
-  case class Call(calle: Expr, name: Token, args: List[Expr] = List.empty)
-      extends Expr
+  case class Call(calle: Expr, name: Token, args: List[Expr] = List.empty) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitCall(this)
+  }
 
   // 123, true, false
-  case class Integer(value: Int) extends Expr
-  case class Bool(value: Boolean) extends Expr
+  case class Integer(value: Int) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitInteger(this)
+  }
+  case class Bool(value: Boolean) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitBool(this)
+  }
   // bonus: "foo"?
-  case class String(value: String) extends Expr
+  case class String(value: String) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = ???
+  }
 
   // let foo = ...;
   // foo (used in some context)
-  case class Variable(name: Token) extends Expr
+  case class Variable(name: Token) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitVariable(this)
+  }
 
   // this (used in some context)
-  case class This(keyword: Token) extends Expr
+  case class This(keyword: Token) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitThis(this)
+  }
 
   // new Foo[<amount>]
-  case class NewArray(typ: Token, count: Expr) extends Expr
+  case class NewArray(typ: Token, count: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitNewArray(this)
+  }
 
   // new Foo(...)
-  case class NewInstance(typ: Token, args: List[Expr] = List.empty) extends Expr
+  case class NewInstance(typ: Token, args: List[Expr] = List.empty) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitNewInstance(this)
+  }
 
   // <op>foo, !foo, -foo
-  case class Unary(op: UnaryOp, left: Expr) extends Expr
+  case class Unary(op: UnaryOp, left: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitUnary(this)
+  }
 
   // (foo)
-  case class Grouping(inner: Expr) extends Expr
+  case class Grouping(inner: Expr) extends Expr {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitGrouping(this)
+  }
 }
--- src/main/scala/ast/Stmt.scala
+++ src/main/scala/ast/Stmt.scala
@@ -5,47 +5,93 @@ import tokenizer.Token
 
 /** Things that modify state or declare things
   */
-sealed abstract class Stmt
+sealed abstract class Stmt {
+  def accept[R](visitor: Stmt.Visitor[R]): R
+}
 
 object Stmt {
+  trait Visitor[R] {
+    def visitClass(stmt: Class): R = ???
+    def visitMainClass(stmt: MainClass): R = ???
+    def visitMethod(stmt: Method): R = ???
+    def visitMainMethod(stmt: MainMethod): R = ???
+    def visitProperty(stmt: Property): R = ???
+    def visitBlock(stmt: Block): R = ???
+    def visitAssign(stmt: Assign): R = ???
+    def visitIf(stmt: If): R = ???
+    def visitWhile(stmt: While): R = ???
+    def visitPrint(stmt: Print): R = ???
+    def visitIndexSet(stmt: IndexSet): R = ???
+    def visitReturn(stmt: Return): R = ???
+  }
+
   // Declarations
   case class Class(
       name: Token,
       parentClass: Option[Expr.Variable] = None,
       methods: List[Method] = List.empty,
       properties: List[Property] = List.empty
-  ) extends Stmt
+  ) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitClass(this)
+  }
 
-  case class MainClass(name: Token, body: MainMethod) extends Stmt
+  case class MainClass(name: Token, body: MainMethod) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitMainClass(this)
+  }
 
-  case class Method(returnType: Type, name: Token, arguments: List[Argument] = List.empty, body: Block)
-      extends Stmt
+  case class Method(
+      returnType: Type,
+      name: Token,
+      arguments: List[Argument] = List.empty,
+      body: Block
+  ) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitMethod(this)
+  }
 
-  case class MainMethod(argName: Token, body: Block) extends Stmt
+  case class MainMethod(argName: Token, body: Block) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitMainMethod(this)
+  }
 
-  case class Property(typ: Type, name: Token, assigned: Option[Expr] = None) extends Stmt
+  // TODO: Rename because we also use these in methods
+  case class Property(typ: Type, name: Token, assigned: Option[Expr] = None) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitProperty(this)
+  }
 
   // Other
   // { <stmt>* }
-  case class Block(statements: List[Stmt] = List.empty) extends Stmt
+  case class Block(statements: List[Stmt] = List.empty) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitBlock(this)
+  }
 
-  case class Assign(name: Token, assigned: Expr) extends Stmt
+  case class Assign(name: Token, assigned: Expr) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitAssign(this)
+  }
 
   // if (<cond>) <stmt>
   case class If(
       condition: Expr,
       thenBranch: Stmt,
       elseBranch: Option[Stmt] = None
-  ) extends Stmt
+  ) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitIf(this)
+  }
 
   // while (<condition>) <stmt>
-  case class While(condition: Expr, body: Stmt) extends Stmt
+  case class While(condition: Expr, body: Stmt) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitWhile(this)
+  }
 
   // System.out.println(<expr>)
-  case class Print(expression: Expr) extends Stmt
+  case class Print(expression: Expr) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitPrint(this)
+  }
 
   // foo["bar"] = baz;
-  case class IndexSet(obj: Token, index: Expr, value: Expr) extends Stmt
+  case class IndexSet(obj: Token, index: Expr, value: Expr) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitIndexSet(this)
+  }
 
-  case class Return(expr: Expr) extends Stmt
+  case class Return(expr: Expr) extends Stmt {
+    override def accept[R](visitor: Visitor[R]): R = visitor.visitReturn(this)
+  }
 }
