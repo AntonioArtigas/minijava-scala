eb878263247ca7c556efea60d1f2b8200a4ac0fc
AntonioArtigas <48927276+AntonioArtigas@users.noreply.github.com>
2022-10-29 19:51:54 -0400

    Allow `-` as a unary operator.

--- src/main/scala/ast/UnaryOp.scala
+++ src/main/scala/ast/UnaryOp.scala
@@ -7,11 +7,12 @@ object UnaryOp {
   def fromTokenType(typ: TokenType): UnaryOp = {
     typ match {
       case TokenType.BANG => BANG
+      case TokenType.DASH => NEGATE
       case _ => ???
     }
   }
 }
 
 enum UnaryOp {
-  case BANG //, NEGATE
+  case BANG, NEGATE
 }
